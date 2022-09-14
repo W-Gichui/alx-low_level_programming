a3001967c3f03f6f214a4c9308cfe7a1b207c565
GG <wycliffgichui@gmail.com>
2022-09-14 17:16:38 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha:- if letter is a letter
+ * Return: 1 if lowercase 0 if otherwise
+ * @c: argument to be checked
+ */
+int _isalpha(int c)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
